795c6a05c8c74d92e3a031e779b5332ad1d2f42e
Jesus Casado <computadoranegra901@gmail.com>
2023-03-23 13:42:19 -0700

    septimo commit de seguridad

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -1,13 +1,17 @@
-window.addEventListener("load", () => {
-	pokedexOpen.classList.add("active");
-});
 const pokedexOpen = document.getElementById("open");
-
+const modal = document.getElementById("modal");
+const modalBtn = document.getElementById("modal-btn");
+const welcome = document.getElementById("welcome");
 const screenPokemon = document.getElementById("screen");
 const miniDisplay = document.getElementById("mini-display");
 const infoScreen = document.getElementById("info-screen");
 const buttonOn = document.getElementById("on");
 
+modalBtn.addEventListener("click", () => {
+	modal.classList.add("out");
+	welcome.classList.add("out");
+	pokedexOpen.classList.add("active");
+});
 buttonOn.addEventListener("click", () => {
 	buttonOn.classList.toggle("active");
 	screenPokemon.classList.toggle("active");
