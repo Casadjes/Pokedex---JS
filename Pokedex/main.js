0adfa3c489a097dc3da4ea3a68289ee22ea034e9
Jesus Casado <computadoranegra901@gmail.com>
2023-03-23 12:02:49 -0700

    sexto commit de seguridad

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -1,5 +1,16 @@
-const pokedexOpen = document.getElementById("open");
-
 window.addEventListener("load", () => {
 	pokedexOpen.classList.add("active");
+});
+const pokedexOpen = document.getElementById("open");
+
+const screenPokemon = document.getElementById("screen");
+const miniDisplay = document.getElementById("mini-display");
+const infoScreen = document.getElementById("info-screen");
+const buttonOn = document.getElementById("on");
+
+buttonOn.addEventListener("click", () => {
+	buttonOn.classList.toggle("active");
+	screenPokemon.classList.toggle("active");
+	miniDisplay.classList.toggle("active");
+	infoScreen.classList.toggle("active");
 });
